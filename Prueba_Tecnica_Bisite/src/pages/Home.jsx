6df51ce23dd1e64f8a89d48f578a610cc14b2a20
Juli-VC <126793927+Juli-VC@users.noreply.github.com>
2025-02-20 21:14:45 +0100

    feat(app): Complete exercise. Components, Home page, Dinamic color palette, etc

--- Prueba_Tecnica_Bisite/src/pages/Home.jsx
+++ Prueba_Tecnica_Bisite/src/pages/Home.jsx
@@ -1,33 +1,75 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
 import { BigCard } from "../components/home/BigCard"
-
+import { themeWeb } from "../themes/theme"
+import Grid from '@mui/material/Grid2';
+import iconSatellite from '../assets/sattelite-sYRI4arYNs.png'
+import Carousel from "../components/home/CarouselGallery"
+import TableData from "../components/home/TableData"
+import { Benefits } from "../components/home/Benefits"
+import { Documents } from "../components/home/Documents"
+import { Verifications } from "../components/home/Verifications"
+import { Tags } from "../components/home/Tags"
+import { ThreeCards } from "../components/home/ThreeCards";
 
 export const Home = () => {
     return (
         <>
             <BigCard />
 
-            <Box sx={{ padding: "100px" }}>
-                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                    <Box sx={{ height: "150px", width: "300px", background: "#438C40", margin: "20px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                        <Typography>ℹ</Typography>
-                        <Typography>Details info</Typography>
-                    </Box>
-                    <Box sx={{ height: "150px", width: "300px", background: "#438C40", margin: "20px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                        <Typography>🛠</Typography>
-                        <Typography>Monotoring Data</Typography>
-                    </Box>
-                    <Box sx={{ height: "150px", width: "300px", background: "#438C40", margin: "20px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                        <Typography>🎫</Typography>
-                        <Typography>Credit Timeline</Typography>
-                    </Box>
-                </Box>
-                <Typography>
-                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
-                </Typography>
-                <Box >
-                    <img />
-                </Box>
+            <Box sx={{ padding: "20px 200px" }}>
+
+                {/* 3 cards */}
+                <ThreeCards />
+
+                {/* Main Section */}
+                <Grid container spacing={10} px={5} my={6}>
+                    {/* Left Column Section. 1-Photos 2-Text 3-Table */}
+                    <Grid size={{ xs: 12, lg: 9 }}>
+                        <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly", gap: "30px" }}>
+                            {/* Satellite monitoring + Icon */}
+                            <Box>
+                                <Button variant="outlined" sx={{ borderColor: themeWeb.colors.primary, color: themeWeb.colors.primary, borderWidth: "2px" }}>
+                                    <Typography>SATELLITE MONITORING</Typography>
+                                    <img alt="satellite monitoring" src={iconSatellite} />
+                                </Button>
+                            </Box>
+                            {/* 2 photos -> x4 gallery */}
+                            <Box>
+                                <Carousel />
+                            </Box>
+                            {/* Main Text */}
+                            <Typography sx={{ textAlign: "justify" }}>
+                                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                            </Typography>
+                            {/* Tabla data */}
+                            <Box >
+                                <TableData />
+                            </Box>
+                            {/* Benefits section */}
+                            <Box >
+                                <Benefits />
+                            </Box>
+                            {/* Documents */}
+                            <Box >
+                                <Documents />
+                            </Box>
+                        </Box>
+                    </Grid>
+
+                    {/* Right Column Section. Verification and Tags */}
+                    <Grid size={{ xs: 12, lg: 3 }} >
+                        {/* Verification */}
+                        <Box>
+                            <Verifications />
+                        </Box>
+                        {/* Tags */}
+                        <Box >
+                            <Tags />
+                        </Box>
+                    </Grid>
+                </Grid>
+
+
             </Box>
         </>
     )
